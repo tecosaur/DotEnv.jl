94a19414507a3616e3b6bb912f7c59ebd3604208
TEC <git@tecosaur.net>
2023-11-20 00:52:05 +0800

    Support loading and unloading env files

--- src/DotEnv.jl
+++ src/DotEnv.jl
@@ -21,8 +21,20 @@ Base.length(eo::EnvOverlay) = length(eo.overlay)
 Base.iterate(eo::EnvOverlay) = iterate(eo.overlay)
 Base.iterate(eo::EnvOverlay, i) = iterate(eo.overlay, i)
 
-const ENV_STACKS = IdDict{AbstractDict{String, String}, Vector{Pair{String, Vector{Pair{String, Tuple{String, Bool}}}}}}()
-const ENV_ORIGINALS = IdDict{AbstractDict{String, String}, Dict{String, String}}()
+struct EnvEntry
+    key::String
+    value::String
+    interpolate::Bool
+end
+
+struct EnvFile
+    path::String
+    entries::Vector{EnvEntry}
+    override::Bool
+end
+
+const ENV_STACKS = IdDict{AbstractDict{String, String}, Vector{EnvFile}}()
+const ENV_ORIGINALS = IdDict{AbstractDict{String, String}, Dict{String, Union{String, Nothing}}}()
 
 """
     _parse(source::Union{IO, AbstractString, AbstractVector{UInt8}})
@@ -30,7 +42,7 @@ const ENV_ORIGINALS = IdDict{AbstractDict{String, String}, Dict{String, String}}
 Parse the `.env` content `source` into a vector of `Pair{String, Tuple{String, Bool}}` key-value-interpolate pairs.
 """
 function _parse(src::IO)
-    results = Pair{String, Tuple{String, Bool}}[]
+    results = EnvEntry[]
     for line in eachline(src)
         keyval = tryparseline(line)
         !isnothing(keyval) && push!(results, keyval)
@@ -47,7 +59,7 @@ _parse(src::AbstractVector{UInt8}) = _parse(IOBuffer(src))
 Parse the `.env` content `source` into a `Dict{String, String}`.
 """
 parse(src::Union{<:IO, <:AbstractString, <:AbstractVector{UInt8}}) =
-    Dict{String, String}(Iterators.map(kqv -> first(kqv) => first(last(kqv)), _parse(src)))
+    Dict{String, String}(Iterators.map(e -> e.key => e.value, _parse(src)))
 
 """
     tryparseline(line::AbstractString)
@@ -69,14 +81,15 @@ function tryparseline(line::AbstractString)
     value = tryreadvalue(@view sline[valstart:end])
     isnothing(value) && return # malformed_value
     key = String(@view sline[1:keyend-1])
-    key => value
+    EnvEntry(key, first(value), last(value))
 end
 
 """
     tryreadvalue(valstring::AbstractString)
 
 Try to extract the possibly quoted value from `valstring`, which may be
-succeeded by a #-comment.
+succeeded by a #-comment. Returns a tuple of the value and a `Bool` indicating
+whether or not the value should be interpolated.
 """
 function tryreadvalue(valstring::AbstractString)
     isempty(valstring) && return ("", true)
@@ -116,11 +129,11 @@ function tryreadvalue(valstring::AbstractString)
     end
 end
 
-function loadexpand!(dotenv::Dict{String, String}, (key, (val, interp))::Pair{String, Tuple{String, Bool}}, fallback::AbstractDict{String, String}=ENV)
-    dotenv[key] = if !interp || !occursin('$', val)
-        val
+function loadexpand!(dotenv::Dict{String, String}, entry::EnvEntry, fallback::AbstractDict{String, String}=ENV)
+    dotenv[entry.key] = if !entry.interpolate || !occursin('$', entry.value)
+        entry.value
     else
-        interpolate(val, dotenv, fallback)
+        interpolate(entry.value, dotenv, fallback)
     end
     dotenv
 end
@@ -200,40 +213,102 @@ expanding interpolated values with `env`.
 
 Should the file `path` exist, an empty `EnvOverlay` is silently returned.
 """
-function config(entries::Vector{Pair{String, Tuple{String, Bool}}}; env::AbstractDict{String, String} = ENV)
+function config(entries::Vector{EnvEntry}; env::AbstractDict{String, String} = ENV, override::Bool = false)
     dotenv = Dict{String, String}()
     for entry in entries
-        loadexpand!(dotenv, entry, env)
+        if override || !haskey(env, entry.key)
+            loadexpand!(dotenv, entry, env)
+        end
     end
     EnvOverlay(env, dotenv)
 end
 
-config(src::IO; env::AbstractDict{String, String} = ENV) =
-    config(_parse(src); env)
+config(src::IO; env::AbstractDict{String, String} = ENV, override::Bool = false) =
+    config(_parse(src); env, override)
 
-function config(path::AbstractString = ".env"; env::AbstractDict{String, String} = ENV)
-    !isfile(path) && return EnvOverlay(env, Dict{String, String}())
-    config(open(_parse, src); env)
+function config(path::AbstractString = ".env"; env::AbstractDict{String, String} = ENV, override::Bool = false)
+    isfile(path) || return EnvOverlay(env, Dict{String, String}())
+    config(open(_parse, path); env, override)
 end
 
 """
-    load(path::AbstractString; override::Bool=false, env = ENV)
+    load!([env=ENV], path::AbstractString = ".env"; override::Bool=false)
 
 Read the `.env` file `path`, parse its content, and store the result to `env`.
 Should `override` be set, values already present in `env` will be replaced with
 statements from `path`.
 """
-function load(path::AbstractString = ".env"; override::Bool=false, env::AbstractDict{String, String} = ENV)
-    if !isfile(path)
-        @warn "Dotenv file '$path' does not exist"
-        return EnvDict(Dict{String, String}())
-    end
-    cfg = config(path, env)
-    for (key, val) in cfg.dict
-        if !haskey(env, key) || override
+function load!(env::AbstractDict{String, String}, path::AbstractString = ".env"; override::Bool=false)
+    isfile(path) || return EnvOverlay(env, Dict{String, String}())
+    unload!(env, path)
+    entries = open(_parse, path)
+    orig = get!(() -> Dict{String, Union{String, Nothing}}(), ENV_ORIGINALS, env)
+    stack = get!(() -> Pair{String, Vector{Pair{String, Tuple{String, Bool}}}}[], ENV_STACKS, env)
+    push!(stack, EnvFile(abspath(path), entries, override))
+    cfg = config(entries; env, override)
+    for (key, val) in cfg.overlay
+        if !haskey(env, key)
+            orig[key] = nothing
+            env[key] = val
+        elseif override
+            get!(orig, key, env[key])
             env[key] = val
         end
     end
+end
+
+load!(path::AbstractString = ".env"; override::Bool=false) = load!(ENV, path; override)
+
+"""
+    unload!(env::AbstractDict{String, String}, path::AbstractString)
+    unload!(ENV, path::AbstractString = ".env")
+
+Unload the dotenv file `path` from `env`. When `env` is omitted, `ENV` is used
+and a default `path` of `.env` is used.
+"""
+function unload!(env::AbstractDict{String, String}, path::AbstractString)
+    uabspath = abspath(path)
+    (!haskey(ENV_STACKS, env) || !any(e -> e.path == uabspath, ENV_STACKS[env])) && return env
+    stack = deepcopy(ENV_STACKS[env])
+    unload!(env)
+    orig = ENV_ORIGINALS[env] = Dict{String, Union{String, Nothing}}()
+    newstack = ENV_STACKS[env] = Vector{EnvFile}()
+    for envfile in stack
+        envfile.path == uabspath && continue
+        push!(newstack, envfile)
+        cfg = config(envfile.entries; env, override=envfile.override)
+        for (key, val) in cfg.overlay
+            if !haskey(env, key)
+                orig[key] = nothing
+                env[key] = val
+            elseif envfile.override
+                get!(orig, key, env[key])
+                env[key] = val
+            end
+        end
+    end
+    env
+end
+
+unload!(path::AbstractString = ".env") = unload!(ENV, path)
+
+"""
+    unload!(env::AbstractDict{String, String})
+
+Unload all dotenv modifications to `env`.
+"""
+function unload!(env::AbstractDict{String, String})
+    orig = get!(() -> Dict{String, Union{String, Nothing}}(), ENV_ORIGINALS, env)
+    for (key, val) in orig
+        if val isa String
+            env[key] = val
+        else # isa Nothing
+            delete!(env, key)
+        end
+    end
+    delete!(ENV_ORIGINALS, env)
+    delete!(ENV_STACKS, env)
+    env
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,98 +1,51 @@
 module TestDotEnv
 
 using Test
-using DotEnv: parse, _parse, tryparseline, interpolate, load
+using DotEnv: DotEnv, EnvEntry, parse, _parse, tryparseline, interpolate, config, load!, unload!
 
 # There is no "USER" variable on windows.
 the_user = haskey(ENV, "USER") ? ENV["USER"] : "WINDOWS"
 ENV["USER"] = the_user
 
-@testset "basic" begin
-    #basic input
-    overfile = joinpath(@__DIR__, ".env.override")
-    envfile = joinpath(@__DIR__, ".env")
+cd(@__DIR__)
 
-    #iobuffer, string, overfile
-    @test _parse("BASIC=basic") == ["BASIC" => ("basic", true)]
-    @test _parse(read(overfile)) == ["USER" => ("replaced value", true), "CUSTOMVAL123" => ("yes", true)]
-    @test load(overfile).dict == Dict("USER"=>"replaced value", "CUSTOMVAL123"=>"yes")
-
-    if VERSION >= v"1.7"
-        @test_warn "does not exist" load("nonexistentfile.env")
-    end
-
-    #length of returned values
-    @test length(load(envfile).dict) == 10
-
-    #shouldn't replace ENV vars
-    cfg = load(overfile)
-
-    @test ENV["USER"] != cfg["USER"]
-    @test ENV["USER"] == the_user
-
-    #appropiately loaded into ENV if CUSTOM_VAL is non existent
-    @test ENV["CUSTOMVAL123"] == "yes"
-
-    # Test that EnvOverlay is reading from ENV
-    ENV["SOME_RANDOM_KEY"] = "abc"
-    cfg = load(overfile)
-    @test !haskey(cfg.dict, "SOME_RANDOM_KEY")
-    @test cfg["SOME_RANDOM_KEY"] == "abc"
-    @test get(cfg, "OTHER_RANDOM_KEY", "zxc") == "zxc"
-end
-
-@testset "Override" begin
-    #basic input
-    overfile = joinpath(@__DIR__, ".env.override")
-
-    # Can force override
-    cfg = load(overfile, override=true)
-    @test ENV["USER"] == cfg["USER"]
-    @test ENV["USER"] == "replaced value"
-    
-    # Restore previous environment
-    ENV["USER"] = the_user
-end
-
-
-@testset "parsing" begin
-
+@testset "Parsing" begin
     #comment
-    @test _parse("#HIMOM") isa Vector{Pair{String, Tuple{String, Bool}}}
+    @test _parse("#HIMOM") isa Vector{EnvEntry}
     @test parse("#HIMOM") isa Dict{String, String}
     @test tryparseline("#HIMOM") === nothing
 
     #spaces without quotes
-    @test count(' ', first(last(tryparseline("TEST=hi  the  re")))) == 4
+    @test count(' ', tryparseline("TEST=hi  the  re").value) == 4
 
     #single quotes
-    @test tryparseline("TEST=''") == ("TEST" => ("", false))
-    @test tryparseline("TEST='something'") == ("TEST" => ("something", false))
+    @test tryparseline("TEST=''") == EnvEntry("TEST", "", false)
+    @test tryparseline("TEST='something'") == EnvEntry("TEST", "something", false)
 
     #double quotes
-    @test tryparseline("TEST=\"\"") == ("TEST" => ("", true))
-    @test tryparseline("TEST=\"something\"") == ("TEST" => ("something", true))
+    @test tryparseline("TEST=\"\"") == EnvEntry("TEST", "", true)
+    @test tryparseline("TEST=\"something\"") == EnvEntry("TEST", "something", true)
 
     #inner quotes are mantained
-    @test tryparseline("TEST='\"json\"'") == ("TEST" => ("\"json\"", false))
-    @test tryparseline("TEST=\"'json'\"") == ("TEST" => ("'json'", true))
-    @test tryparseline("TEST='\"'") == ("TEST" => ("\"", false))
-    @test tryparseline("TEST=\"'\"") == ("TEST" => ("'", true))
+    @test tryparseline("TEST='\"json\"'") == EnvEntry("TEST", "\"json\"", false)
+    @test tryparseline("TEST=\"'json'\"") == EnvEntry("TEST", "'json'", true)
+    @test tryparseline("TEST='\"'") == EnvEntry("TEST", "\"", false)
+    @test tryparseline("TEST=\"'\"") == EnvEntry("TEST", "'", true)
 
     #line breaks
-    @test tryparseline("TEST=\"\\n\"") == ("TEST" => ("\n", true)) # It's empty because of final trim
-    @test tryparseline("TEST=\"\\n\\nsomething\"") == ("TEST" => ("\n\nsomething", true))
-    @test tryparseline("TEST=\"something\\nsomething\"") == ("TEST" => ("something\nsomething", true))
-    @test tryparseline("TEST=\"something\\n\\nsomething\"") == ("TEST" => ("something\n\nsomething", true))
-    @test tryparseline("TEST='\\n'") == ("TEST" => ("\\n", false))
-    @test tryparseline("TEST=\\n") == ("TEST" => ("\\n", true))
+    @test tryparseline("TEST=\"\\n\"") == EnvEntry("TEST", "\n", true) # It's empty because of final trim
+    @test tryparseline("TEST=\"\\n\\nsomething\"") == EnvEntry("TEST", "\n\nsomething", true)
+    @test tryparseline("TEST=\"something\\nsomething\"") == EnvEntry("TEST", "something\nsomething", true)
+    @test tryparseline("TEST=\"something\\n\\nsomething\"") == EnvEntry("TEST", "something\n\nsomething", true)
+    @test tryparseline("TEST='\\n'") == EnvEntry("TEST", "\\n", false)
+    @test tryparseline("TEST=\\n") == EnvEntry("TEST", "\\n", true)
 
     #empty vars
-    @test tryparseline("TEST=") == ("TEST" => ("", true))
+    @test tryparseline("TEST=") == EnvEntry("TEST", "", true)
 
     #trim spaces without quotes
-    @test tryparseline("TEST=  something  ") == ("TEST" => ("something", true))
-    @test tryparseline("TEST=    ") == ("TEST" => ("", true))
+    @test tryparseline("TEST=  something  ") == EnvEntry("TEST", "something", true)
+    @test tryparseline("TEST=    ") == EnvEntry("TEST", "", true)
 end
 
 @testset "Interpolation" begin
@@ -106,6 +59,127 @@ end
     @test interpolate("hello \${USERR:-\$USER}", Dict{String, String}(), ENV) == "hello $the_user"
     @test interpolate("hello \${USERR:-\${USERR:-you}}", Dict{String, String}(), ENV) == "hello you"
     @test interpolate("hello \${USERR:-\${USERR:-\$USER}}", Dict{String, String}(), ENV) == "hello $the_user"
+end
+
+@testset "Basic" begin
+    #iobuffer, string, .env.override
+    @test _parse("BASIC=basic") == [EnvEntry("BASIC", "basic", true)]
+    @test _parse(read(".env.override")) == [EnvEntry("USER", "replaced value", true), EnvEntry("CUSTOMVAL123", "yes", true)]
+    @test config(".env.override").overlay == Dict("CUSTOMVAL123"=>"yes")
+    @test config(".env.override", override=true).overlay == Dict("USER"=>"replaced value", "CUSTOMVAL123"=>"yes")
+
+    @test config("nonexistentfile.env") isa Any # No error
+
+    #length of returned values
+    @test length(config(".env", override=true).overlay) == 10
+
+    #shouldn't replace ENV vars
+    cfg = config(".env.override")
+    @test ENV["USER"] == cfg["USER"] == the_user
+
+    # Now with override
+    cfg = config(".env.override", override=true)
+
+    @test ENV["USER"] != cfg["USER"]
+    @test ENV["USER"] == the_user
+
+    #appropiately loaded into cfg if CUSTOM_VAL is non existent
+    @test cfg["CUSTOMVAL123"] == "yes"
+
+    # Test that EnvOverlay is reading from ENV
+    ENV["SOME_RANDOM_KEY"] = "abc"
+    cfg = config(".env.override")
+    @test !haskey(cfg.overlay, "SOME_RANDOM_KEY")
+    @test cfg["SOME_RANDOM_KEY"] == "abc"
+    @test get(cfg, "OTHER_RANDOM_KEY", "zxc") == "zxc"
+end
+
+@testset "Load and unload" begin
+    virginenv = Dict{String, String}(ENV)
+    envvals = config(".env", env=Dict{String, String}())
+    overridevals = config(".env.override", env=Dict{String, String}())
+    myenv = Dict{String, String}()
+    load!(myenv, ".env")
+    @test length(DotEnv.ENV_STACKS[myenv]) == 1
+    for key in keys(envvals)
+        @test myenv[key] == envvals[key]
+    end
+    load!(myenv, ".env.override")
+    @test length(DotEnv.ENV_STACKS[myenv]) == 2
+    for key in keys(overridevals)
+        if haskey(envvals, key)
+            @test myenv[key] == envvals[key]
+        else
+            @test myenv[key] == overridevals[key]
+        end
+    end
+    load!(myenv, ".env.override", override=true)
+    @test length(DotEnv.ENV_STACKS[myenv]) == 2
+    for key in keys(overridevals)
+        if haskey(overridevals, key)
+            @test myenv[key] == overridevals[key]
+        else
+            @test myenv[key] == envvals[key]
+        end
+    end
+    unload!(myenv, ".env")
+    @test length(DotEnv.ENV_STACKS[myenv]) == 1
+    for key in keys(envvals)
+        if haskey(overridevals, key)
+            @test myenv[key] == overridevals[key]
+        else
+            @test !haskey(ENV, key)
+        end
+    end
+    unload!(myenv, ".env.override")
+    @test isempty(myenv)
+    # Now with `ENV`
+    load!(".env")
+    for key in keys(envvals)
+        if haskey(virginenv, key)
+            @test ENV[key] == virginenv[key]
+        else
+            @test ENV[key] == envvals[key]
+        end
+    end
+    load!(".env.override")
+    for key in keys(overridevals)
+        if haskey(virginenv, key)
+            @test ENV[key] == virginenv[key]
+        elseif haskey(envvals, key)
+            @test ENV[key] == envvals[key]
+        else
+            @test ENV[key] == overridevals[key]
+        end
+    end
+    load!(".env.override", override=true)
+    for key in keys(overridevals)
+        if haskey(overridevals, key)
+            @test ENV[key] == overridevals[key]
+        elseif haskey(virginenv, key)
+            @test ENV[key] == virginenv[key]
+        else
+            @test ENV[key] == envvals[key]
+        end
+    end
+    unload!(".env")
+    for key in keys(envvals)
+        if haskey(overridevals, key)
+            @test ENV[key] == overridevals[key]
+        elseif haskey(virginenv, key)
+            @test ENV[key] == virginenv[key]
+        else
+            @test !haskey(ENV, key)
+        end
+    end
+    unload!(".env.override")
+    for key in keys(overridevals)
+        if !haskey(virginenv, key)
+            @test !haskey(ENV, key)
+        end
+    end
+    @test length(ENV) == length(virginenv)
+    @test Dict{String, String}(ENV) == virginenv
 end
 
 end
